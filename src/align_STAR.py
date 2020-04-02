2795cf7b74f4487873ab6e32d615992c41d5f793
Jessica Mattick <jmattick16@gmail.com>
2020-04-02 10:56:26 -0500

    remove zcat parameter

--- src/align_STAR.py
+++ src/align_STAR.py
@@ -30,7 +30,7 @@ else:
             r1 = str(fastq_dir) + str(base) + '_1.fastq' #path to read 1
             r2 = str(fastq_dir) + str(base) + '_2.fastq' #path to read 2
             outname = base.split('/')[-1] #get name of file
-            cmd = 'STAR --runThreadN 4 --genomeDir ' + str(idx) + ' --genomeLoad NoSharedMemory --outFileNamePrefix ' + str(output) + str(outname) +  '_ --outReadsUnmapped Fastx --outSAMtype BAM SortedByCoordinate --outSAMstrandField intronMotif --outSAMattributes All --readFilesCommand zcat --outFilterType BySJout --outFilterMultimapNmax 1 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --readFilesIn ' + str(r1) + ' ' + str(r2) # STAR command
+            cmd = 'STAR --runThreadN 4 --genomeDir ' + str(idx) + ' --genomeLoad NoSharedMemory --outFileNamePrefix ' + str(output) + str(outname) +  '_ --outReadsUnmapped Fastx --outSAMtype BAM SortedByCoordinate --outSAMstrandField intronMotif --outSAMattributes All --outFilterType BySJout --outFilterMultimapNmax 1 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --readFilesIn ' + str(r1) + ' ' + str(r2) # STAR command
             print('Aligning reads with STAR')
             print(cmd)
             os.system(cmd)
