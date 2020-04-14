9e45d88c233a8ce081934dddde53bb4fb5f178c3
xudeng2008 <58059010+xudeng2008@users.noreply.github.com>
2020-04-14 16:21:09 -0500

    Update index_genome_bwa.py

--- src/index_genome_bwa.py
+++ src/index_genome_bwa.py
@@ -5,21 +5,24 @@ import sys
 
 params = sys.argv # get parameters
 
-fasta = None # path to genome fasta file
+fasta_dir = None # path to genome fasta file
 
 # Loop through parameters
 for i in range(len(params)-1):
-    if params[i] == '-f' or params[i] == '--fasta':
+    if params[i] == '-f' or params[i] == '--fasta_dir':
         fasta = params[i+1]
-    elif params[i] == '-a' or params[i] == '--gtf_annotation':
-        gtf = params[i+1]
-    elif params[i] == '-o' or params[i] == '--output':
-        output = params[i+1]
-        if not output.endswith('/'):
-            output = output + '/'
 
-print('Indexing genome: ')
-cmd = 'bwa index ' + str(fasta)
-print(cmd)
-
-os.system(str(cmd))
+# Confirm parameters set
+if fasta_dir == None:
+        print('Error: Invalid input parameters. \nSet fasta directory with -f or --fasta_dir.')
+        sys.exit()
+else:
+    for file in os.listdir(str(fasta_dir)): #loop through files in data directory
+        if file.endswith('.fa'): #if file is first read in pair
+            base = file.split('.fa')[0] #get basename of file
+            r = str(fasta_dir) + str(base) + '.fa' #path to genome file
+            
+    print('Indexing genome for BWA: ')
+    cmd = 'bwa index ' + r
+    print(cmd)
+    os.system(str(cmd))
