e435f82af7ad6626f49a8e100834679f88ea16e5
Jessica Mattick <jmattick16@gmail.com>
2020-04-13 20:53:37 -0500

    add alignment commands to main pipeline

--- main_pipeline.py
+++ main_pipeline.py
@@ -45,7 +45,11 @@ if ref_urls is None or output is None or rna_acc is None or dna_acc is None:
 
 # Create Directories
 rna_fastq = output + 'rna_fastq/' # set path to directory containing rna fastq files
+rna_fastq_trimmed = rna_fastq + 'trimmed/' # set path to directory containing trimmed rna fastq files
+rna_bam = output + 'rna_bam/' # set path to directory containing rna bam files
+dna_bam = output + 'dna_bam/' #set path to directory containing dna bam files
 dna_fastq = output + 'dna_fastq/' #set path to directory containing dna fastq files
+fastqc = output + 'fastqc/' # set path to directory containing fastqc reports
 reference = output + 'reference/' # set path to directory containing reference information
 star_index = reference + 'STAR_index/'
 
@@ -72,6 +76,16 @@ cmd = 'python3 src/get_SRA_reads.py -a ' + str(dna_acc) + ' -o ' + str(dna_fastq
 os.system('date')
 os.system('echo ' + cmd)
 
+# Quality Check RNAseq Reads
+cmd = 'python3 src/fastqc.py -f ' + str(rna_fastq) + ' -o ' + str(fastqc)
+os.system('date')
+os.system('echo ' + cmd)
+
+# Quality Check DNAseq Reads
+cmd = 'python3 src/fastqc.py -f ' + str(dna_fastq) + ' -o ' + str(fastqc)
+os.system('date')
+os.system('echo ' + cmd)
+
 # Index genome BWA
 cmd = 'python3 src/index_genome_bwa.py ' + str(genome) + '*.fa'
 os.system('date')
@@ -87,5 +101,13 @@ cmd = 'python3 src/align_BWA.py -fq ' + str(dna_fastq) + ' -fa ' + str(genome)
 os.system('date')
 os.system('echo ' + cmd)
 
+# Quality trime RNA Reads
+cmd = 'python3 src/fastp.py -f ' + str(rna_fastq) + ' -o ' + str(rna_fastq_trimmed) 
+os.system('date')
+os.system('echo ' + cmd)
 
+# Align Reads STAR
+cmd = 'python3 src/align_STAR.py -f ' + str(rna_fastq_trimmed) + ' -g ' + str(star_index) + ' -o ' + str(rna_bam)
+os.system('date')
+os.system('echo ' + cmd)
 
