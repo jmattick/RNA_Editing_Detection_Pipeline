447d16fb54b28b934173a55e18ccbd4c065993a3
xudeng2008 <58059010+xudeng2008@users.noreply.github.com>
2020-04-19 15:47:20 -0500

    Update align_BWA.py

--- src/align_BWA.py
+++ src/align_BWA.py
@@ -5,18 +5,18 @@ import sys
 
 params = sys.argv #input parameters
 
-fastq_dir = None # path to directory containing fastq files
+fastq_dir = None # path to directory containing WGS fastq files
 fasta_dir = None # path to directory containing genome fasta file
 
 for i in range(len(params)-1): #loop through parameters
-    if params[i] == '-fq' or params[i] == '--fastq_dir': #if parameter is fastq directory
+    if params[i] == '-fq' or params[i] == '--fastq_dir': #if parameter is WGS fastq directory
         fastq_dir = params[i + 1] #set email
     elif params[i] == '-fa' or params[i] == '--fasta_dir': #if parameter is genome index
         fasta_dir = params[i+1]
     
 
 if fastq_dir == None or fasta_dir == None:
-    print('Error: Invalid input parameters. \nSet fastq directory with -f or --fastq_dir.\nSet path to directory containing genome fasta file with -fa or --fasta_dir.')
+    print('Error: Invalid input parameters. \nSet WGS fastq directory with -f or --fastq_dir.\nSet path to directory containing genome fasta file with -fa or --fasta_dir.')
 
 else:
 
@@ -26,7 +26,7 @@ else:
             r1 = str(fastq_dir) + str(base) + '_1.fastq' #path to read 1
             r2 = str(fastq_dir) + str(base) + '_2.fastq' #path to read 2
             outname = base.split('/')[-1] #get name of file
-            cmd = 'bwa mem -t 4 ' + str(fasta_dir) + ' -Y ' + r1 + ' ' + r2 + ' > ' + str(fasta_dir) + str(outname) + '.sam' # BWA command
+            cmd = 'bwa mem -t 4 ' + str(fasta_dir) + ' -Y ' + r1 + ' ' + r2 + ' > ' + str(fastq_dir) + str(outname) + '.sam' # BWA command
             print('Aligning reads with BWA')
             print(cmd)
             os.system(cmd)
